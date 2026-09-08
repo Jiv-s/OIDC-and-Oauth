9e718e0ca0af4b43070878ed419a3971b97478c5
Jivesh Jadhav <jadhavjivesh32@gmail.com>
2026-09-08 23:57:05 +0530

    feat: add express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("hello")
+import express from "express"
+import cookieParser from "cookie-parser"
+
+const app =express()
+
+//middlwares
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser())
+
+//routers
+app.get("/",(req,res)=>{
+    res.send("OIDC server")
+})
+
+app.listen(3000,()=>{
+    console.log("server started at port 3000")
+})
